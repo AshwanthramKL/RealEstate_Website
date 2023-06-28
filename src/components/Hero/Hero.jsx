2338e9335aa1323187cfa4eea245b60dc62e4b26
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-06-28 22:10:37 +0530

    added animations

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import "./Hero.css";
 import { HiLocationMarker } from "react-icons/hi";
 import CountUp from "react-countup";
+import { motion } from "framer-motion";
+
 const Hero = () => {
   return (
     <section className="hero-wrapper">
@@ -10,10 +12,14 @@ const Hero = () => {
         <div className="flexColStart hero-left">
           <div className="hero-title">
             <div className="orange-circle" />
-            <h1>
+            <motion.h1
+              initial={{ y: "2rem", opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 2, type: "spring" }}
+            >
               Find your <br />
               &emsp; next home
-            </h1>
+            </motion.h1>
           </div>
 
           <div className="flexColStart paddings hero-des">
@@ -62,10 +68,15 @@ const Hero = () => {
 
         {/* right side */}
         <div className="flexCenter hero-right">
-          <div className="image-container">
+          <motion.div
+            initial={{ x: "7rem", opacity: 0 }}
+            animate={{ x: 0, opacity: 1 }}
+            transition={{ duration: 2, type: "spring" }}
+            className="image-container"
+          >
             <img src="./hero-image.png" alt="Hero image" />
             image
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
